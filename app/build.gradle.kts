09eddc65503579cbb03ec068d1aea0e58af75816
prathamngundikere <prathamgundikere@gmail.com>
2025-01-28 21:01:38 +0530

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
     namespace = "com.prathamngundikere.rovervision"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.prathamngundikere.rovervision"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
